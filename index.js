be958e15fa995d10d49a586706c2ea26f2ca3c3d
arbutnar <butnaruarianna@gmail.com>
2024-02-08 11:12:59 +0100

    another commit

--- index.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#profilePicture").addEventListener("click", function() {
-	let	sidebar = document.querySelector(".sidebar");
-
-	if (sidebar.style.visibility === "visible")
-		sidebar.style.visibility = "hidden";
-	else
-		sidebar.style.visibility = "visible";
-});
